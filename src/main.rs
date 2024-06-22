77ca5a409f3da6cc9ecef9687a0bb2a79c65304e
Tushar Dahiya <shreyashsam@gmail.com>
2024-06-22 19:33:23 +0530

    i am back

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
- use std::{net::TcpListener, io::Write};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     println!("Logs from your program will appear here!");
 
-    
-     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
-     for stream in listener.incoming() {
-         match stream {
-             Ok(_stream) => {
-                 println!("accepted new connection");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
-                 stream.write(b"+PONG\r\n").unwrap();
-             }
-             Err(e) => {
-                 println!("error: {}", e);
-             }
-         }
-     }
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+                stream.write(b"+PONG\r\n").unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
